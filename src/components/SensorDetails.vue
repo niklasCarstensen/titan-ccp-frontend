c8397899dc7098e966bbd810aafc1741942f2eeb
Sören Henning <post@soeren-henning.de>
2018-06-04 11:54:25 +0200

    fixed method name

--- src/components/SensorDetails.vue
+++ src/components/SensorDetails.vue
@@ -61,7 +61,7 @@ export default class SensorDetails extends Vue {
 
     @Prop({ required: true }) sensor!: Sensor
 
-    get isAggregated2() {
+    get isAggregated() {
         return this.sensor instanceof AggregatedSensor
     }
 
