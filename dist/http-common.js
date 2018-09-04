a309e423bea6b3333fe128bc8f87b9d580b5791e
Sören Henning <post@soeren-henning.de>
2018-09-04 14:22:13 +0200

    removed compiled files

--- dist/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-export var HTTP = axios.create({
-    //baseURL: `http://jsonplaceholder.typicode.com/`,
-    baseURL: "http://localhost:8080/",
-    headers: {
-        Authorization: 'Bearer {token}'
-    }
-});
-//# sourceMappingURL=http-common.js.map
