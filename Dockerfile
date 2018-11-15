c696c06d2a26525d0189229848aa9a799e4cf59a
Sören Henning <post@soeren-henning.de>
2018-11-15 15:07:08 +0100

    Replace env variables in containers

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,9 @@ FROM nginx:alpine
 COPY index.html /var/www/titan-ccp/index.html
 COPY dist /var/www/titan-ccp/dist
 COPY nginx.conf /etc/nginx/nginx.conf
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Replace env vars in JavaScript files
+echo "Replacing env vars in JS"
+for file in /var/www/titan-ccp/dist/*.js;
+do
+  echo "Processing $file";
+
+  # Use the existing JS file as template
+  if [ ! -f $file.tmpl.js ]; then
+    cp $file $file.tmpl.js
+  fi
+
+  envsubst '$DEMO' < $file.tmpl.js > $file
+done
+
+exec "$@"
