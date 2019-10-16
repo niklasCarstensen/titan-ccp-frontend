f10762470398e59ef6011b13dd144eca281fea09
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2019-10-16 11:40:07 +0200

    Remove merge conflict marker

--- src/Repeater.ts
+++ src/Repeater.ts
@@ -12,13 +12,7 @@ export default class Repeater {
     // TODO remove
     async start(): Promise<void> {
         await this.initialFunction()
-<<<<<<< HEAD
-        this.intervalId = window.setInterval(() => {
-            this.repeatingFunction()
-        }, this.repeatIntervalInMs)
-=======
         this.intervalId = window.setInterval(() => this.repeatingFunction(), this.repeatIntervalInMs)
->>>>>>> 938efde1dc0615aab7a5291c511f3c65aa65dea1
     }
 
     // TODO rename to start
