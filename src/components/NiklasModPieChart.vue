631b4f18279b232c51bafa46a1a0c9dc0f825d34
niklasCarstensen <creeper99.mc@gmail.com>
2019-11-26 13:10:36 +0100

    Add (barely functional) labels

--- src/components/NiklasModPieChart.vue
+++ src/components/NiklasModPieChart.vue
@@ -45,14 +45,16 @@ export default class NiklasModPieChart extends Vue {
       ["Computing Center 3", 212]
     ];
     const data = [123, 125, 137, 135, 189, 123, 146, 146, 134, 124, 134];
-
-    const svg = d3
-      .select(this.$el)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+    const data_labels = [
+      "OwO",
+      "Uwu?",
+      "Öwö",
+      "ÄwÄ",
+      "ÜwÜ",
+      "Ohw",
+      "kek",
+      "kek3"
+    ];
 
     const color = [
       "#66c2a5",
@@ -69,18 +71,28 @@ export default class NiklasModPieChart extends Vue {
       "#ffd92f"
     ];
 
+    const pie_svg = d3
+      .select(this.$el)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    // Create pie structure
     const pie = d3
       .pie()
       .value((Math.PI * 2) / data.length)
       .sort(null);
 
+    // Set Radii
     const maxData = data.reduce((x, y) => Math.max(x, y));
     const arc = d3.arc().innerRadius(0);
     var i = 0;
     arc.outerRadius(d => (data[i++] * height) / 2 / maxData);
 
-    const path = svg.selectAll("path").data(pie(data));
-
+    // Draw pie
+    const path = pie_svg.selectAll("path").data(pie(data));
     path
       .enter()
       .append("path")
@@ -88,6 +100,51 @@ export default class NiklasModPieChart extends Vue {
       .attr("d", <any>arc)
       .attr("stroke", "white")
       .attr("stroke-width", "2px");
+
+    // Create ledgend svg
+    const ledgend_svg = d3
+      .select(this.$el)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", 75)
+      .append("g");
+
+    // Add one dot in the legend for each name.
+    var size = 20;
+    var padding = 75;
+    ledgend_svg
+      .selectAll("mydots")
+      .data(data_labels)
+      .enter()
+      .append("rect")
+      .attr("x", function(d, i) {
+        return 100 + i * (size + padding);
+      })
+      .attr("y", 25) // 100 is where the first dot appears. 25 is the distance between dots
+      .attr("width", size)
+      .attr("height", size)
+      .style("fill", function(d, i) {
+        return color[<any>i];
+      });
+
+    // Add one dot in the legend for each name.
+    ledgend_svg
+      .selectAll("mylabels")
+      .data(data_labels)
+      .enter()
+      .append("text")
+      .attr("x", function(d, i) {
+        return 105 + i * (size + padding) + size;
+      })
+      .attr("y", 27.5 + size / 2) // 100 is where the first dot appears. 25 is the distance between dots
+      .style("fill", function(d, i) {
+        return color[<any>i];
+      })
+      .text(function(d) {
+        return <any>d;
+      })
+      .attr("text-anchor", "left")
+      .style("alignment-baseline", "middle");
 
     this.updateChart();
   }
